560b3a4089836db62cb5daa8ab70214015b69652
deepjoy9 <deepjoysarkar9@gmail.com>
2024-02-09 04:26:10 +0530

    Refactor: Move API keys to .env file for improved security and organization

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCPriQ9-CRfQS_iXhtkE2K1aev7LH3LPxw";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
